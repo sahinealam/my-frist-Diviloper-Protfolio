1701798390dab3d32d5122d6b55c68e0db65f375
unknown <you@example.com>
2025-07-10 08:38:59 +0600

    first commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+const Image =document.getElementsByTagName("img");
+const Text =document.getElementsByTagName("p");
+console.log(Image);
+console.log(Text);
+const htmltag =document.getElementsByTagName("html")[0];
+htmltag.setAttribute('data-theme','light');
